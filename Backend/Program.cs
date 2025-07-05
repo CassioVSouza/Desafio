d5df423afa3d8f97d6bb6ea158b0e0736ffa2d67
Cássio Venâncio de Souza <cassiovenanciosouza@gmail.com>
2025-07-05 13:33:44 -0300

    Swagger adicionado ao backend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,8 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 builder.WebHost.UseUrls("http://0.0.0.0:80");
 
-app.MapGet("/", () => "Hello World!");
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
